3cb13409334fd33a3feebbfb9809aba762b126fa
Peter Fejes <peter.fejes@zeiss.com>
2024-02-16 18:24:19 +0100

    replace redis to postgres

---
+++ dapr/daprState/examples/1_initial/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Dictionary<string, string> repository = new();
+
+Order order = new ("Some value");
+
+repository[order.Id] = order.ToString();
+Console.WriteLine($"Saving Order: {order}");
+
+Order result = Order.CreateFromJson(repository[order.Id]);
+Console.WriteLine($"Getting Order: id:{result.Id} value:{result.Value}");
+
+repository.Remove(order.Id);
+Console.WriteLine($"Deleting Order: {order}");
---
+++ dapr/daprState/examples/3_final/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using Dapr.Client;
+
+var client = new DaprClientBuilder().Build();
+
+Order order = new ("Some value");
+
+string DAPR_STORE_NAME = "statestore";
+
+await client.SaveStateAsync(DAPR_STORE_NAME, order.Id, order);
+Console.WriteLine($"Saving Order: {order}");
+
+Order result = await client.GetStateAsync<Order>(DAPR_STORE_NAME, order.Id);
+Console.WriteLine($"Getting Order: id:{result.Id} value:{result.Value}");
+
+//await client.DeleteStateAsync(DAPR_STORE_NAME, order.Id);
+//Console.WriteLine($"Deleting Order: {order}");
