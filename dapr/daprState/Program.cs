913495e17b1e144aeedf0a565630e4486bee9b4d
Peter Fejes <peter.fejes@zeiss.com>
2024-02-16 17:37:58 +0100

    beauty snipets

--- dapr/daprState/Program.cs
+++ dapr/daprState/Program.cs
@@ -3,12 +3,11 @@ Dictionary<string, string> repository = new();
 
 Order order = new ("Some value");
 
-repository[order.Id.ToString()] = order.ToString();
+repository[order.Id] = order.ToString();
 Console.WriteLine($"Saving Order: {order}");
 
-string result = repository[order.Id.ToString()];
-Order orderLoaded = Order.CreateFromJson(result);
-Console.WriteLine($"Getting Order: {orderLoaded}");
+Order result = Order.CreateFromJson(repository[order.Id]);
+Console.WriteLine($"Getting Order: id:{result.Id} value:{result.Value}");
 
-repository.Remove(order.Id.ToString());
+repository.Remove(order.Id);
 Console.WriteLine($"Deleting Order: {order}");
